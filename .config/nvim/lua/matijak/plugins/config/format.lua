611e39c0f7fc613d49d74a5c80213c0317b3df79
Matija Kukic <kukicmatija@gmail.com>
2024-10-02 09:18:22 +0200

    nvim formatter

---
+++ .config/nvim/lua/matijak/plugins/config/format.lua
@@ -0,0 +1,30 @@
+-- Utilities for creating configurations
+local util = require("formatter.util")
+
+require("formatter").setup({
+	filetype = {
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+        python = {
+            require("formatter.filetypes.python").black,
+        },
+		["*"] = {
+			-- "formatter.filetypes.any" defines default configurations for any
+			-- filetype
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
+})
+
+vim.keymap.set('n', '<leader>fm', ':Format<CR>', { silent = true })
+vim.keymap.set('n', '<leader>fw', ':FormatWrite<CR>', { silent = true })
+
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
