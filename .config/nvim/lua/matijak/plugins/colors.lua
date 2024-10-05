4b015e7f1edef4db266ae8b2dfb6912f6f41fdd2
Matija Kukic <kukicmatija@gmail.com>
2024-10-05 19:00:59 +0200

    refactor, catppuccin

--- .config/nvim/lua/matijak/plugins/colors.lua
+++ .config/nvim/lua/matijak/plugins/colors.lua
@@ -1,35 +1,43 @@
 return {
-    {
-      'sainnhe/everforest',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        --Optionally configure and load the colorscheme
-        --directly inside the plugin declaration.
-        vim.g.everforest_enable_italic = true
-        vim.g.everforest_background = 'hard'
-        vim.o.background = 'dark'
-        vim.cmd.colorscheme('everforest')
-      end
-    },
-    {
-      'sainnhe/gruvbox-material',
-      lazy = false,
-      priority = 1000,
-      --config = function()
-         --Optionally configure and load the colorscheme
-         --directly inside the plugin declaration.
-        --vim.g.gruvbox_material_enable_italic = true
-        ----------------vim.cmd.colorscheme('gruvbox-material')
-      --end
-    },
-    {
-        "folke/tokyonight.nvim",
-        --lazy = false,
-        --priority = 1000,
-        --opts = {},
-        --config = function()
-            --vim.cmd.colorscheme('tokyonight')
-        --end
-    }
+	{
+		"sainnhe/everforest",
+		--lazy = false,
+		--priority = 1000,
+		--config = function()
+		--Optionally configure and load the colorscheme
+		--directly inside the plugin declaration.
+		--	vim.g.everforest_enable_italic = true
+		--	vim.g.everforest_background = "hard"
+		--	vim.o.background = "dark"
+		--	vim.cmd.colorscheme("everforest")
+		--end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		--config = function()
+		--Optionally configure and load the colorscheme
+		--directly inside the plugin declaration.
+		--vim.g.gruvbox_material_enable_italic = true
+		----------------vim.cmd.colorscheme('gruvbox-material')
+		--end
+	},
+	{
+		"folke/tokyonight.nvim",
+		--lazy = false,
+		--priority = 1000,
+		--opts = {},
+		--config = function()
+		--vim.cmd.colorscheme('tokyonight')
+		--end
+	},
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
