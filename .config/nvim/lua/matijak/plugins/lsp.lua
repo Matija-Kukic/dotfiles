e3a77b6e2c8faa2bc9aeed7c286f943d4d076966
Matija Kukic <matija-luka.kukic@fer.hr>
2025-05-21 13:55:01 +0200

    nvim masonlspconfig removed

--- .config/nvim/lua/matijak/plugins/lsp.lua
+++ .config/nvim/lua/matijak/plugins/lsp.lua
@@ -5,15 +5,6 @@ return {
 			require("mason").setup({
 				ensure_installed = {
 					"black",
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
 					"lua_ls",
 					"pylsp",
 					"pyright",
@@ -22,6 +13,7 @@ return {
 			})
 		end,
 	},
+
 	{
 		"L3MON4D3/LuaSnip",
 		version = "v2.*", -- Use the latest released major version
